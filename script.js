b09d0efbee4f871fd50a1a2b6c89c4577440af61
kerry <Careyk896>
2025-06-17 09:29:18 +0100

    fixed js bugs

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){ //wrap entire script in DOMcontentloaded event listener
+/*wrap entire script in DOMcontentloaded event listener*/
+document.addEventListener('DOMContentLoaded', function(){ 
 
-    const form = document.getElementById(registration-form); // store the registration form in  a constant 'form'
-    const feedbackDiv = document.getElementById(form-feedback); // store the feedbackDiv in  a constant 'feedbackDiv'
+    /* select regiatration form and feedbackDiv*/
+    const form = document.getElementById(registration-form); 
+    const feedbackDiv = document.getElementById(form-feedback);
 
+    /*prevent form submission before validation*/
     form.addEventListener('submit', function(event)) {
-        event.preventDefault(); // prevent form submission before validation
+        event.preventDefault(); 
     }
-    let username = document.getElementById(username).value.trim(); // retrieve the value of input fields and trim any whitespace
+
+    /*retrieve and trim user inputs*/
+    let username = document.getElementById(username).value.trim(); 
     let email = document.getElementById(email).value.trim();
     let password = document.getElementById(password).value.trim();
 
+    /*error meesages implementation*/
     let isValid = true;
     let messages = [];
+
+    /*validate username, email and password*/
     if (username.length < 3) {
         isValid = false;
         messages.push("username must be atleast 3 characters!");
     }
+
     if (email.includes('@') && email.includes('.')) {
         isValid = true;
     } else {
         isValid = false;
         messages.push("Enter valid email!");
     }
+
     if (password.length < 8) {
         isValid = false;
         messages.push("password too short");
     }
+
+    /*Display feedback*/
     feedbackDiv.style.display = "block";
+
     if (isValid) {
         feedbackDiv.textContent = "Registration Successful";
-        feedbackDiv.style.color = "#28a745";
+        feedbackDiv.style.color = "#28a745"; // green color
     } else {
         feedbackDiv.innerHTML = messages.join("<br>");
-        feedbackDiv.style.color = "#dc3545"
+        feedbackDiv.style.color = "#dc3545"; // red color
     }
 });
